0b637dcaaf285efb238ff988b4145ab7e9ed23c2
Robert T <robert@tiffert.eu>
2024-03-15 22:11:05 +0000

    Add questions, answer choice and correct answer to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,132 @@
+// Array of questions
+const questions = [{
+        image: "assets/images/ind_sl_a.webp",
+        answers: ["C", "M", "A", "Y"],
+        correctAnswer: "A"
+    },
+    {
+        image: "assets/images/ind_sl_b.webp",
+        answers: ["O", "B", "D", "R"],
+        correctAnswer: "B"
+    },
+    {
+        image: "assets/images/ind_sl_c.webp",
+        answers: ["U", "J", "D", "C"],
+        correctAnswer: "C"
+    },
+    {
+        image: "assets/images/ind_sl_d.webp",
+        answers: ["D", "O", "B", "G"],
+        correctAnswer: "D"
+    },
+    {
+        image: "assets/images/ind_sl_e.webp",
+        answers: ["F", "P", "E", "H"],
+        correctAnswer: "E"
+    },
+    {
+        image: "assets/images/ind_sl_f.webp",
+        answers: ["V", "E", "N", "F"],
+        correctAnswer: "F"
+    },
+    {
+        image: "assets/images/ind_sl_g.webp",
+        answers: ["G", "B", "R", "S"],
+        correctAnswer: "G"
+    },
+    {
+        image: "assets/images/ind_sl_h.webp",
+        answers: ["X", "H", "L", "Q"],
+        correctAnswer: "H"
+    },
+    {
+        image: "assets/images/ind_sl_i.webp",
+        answers: ["J", "Y", "T", "I"],
+        correctAnswer: "I"
+    },
+    {
+        image: "assets/images/ind_sl_j.webp",
+        answers: ["J", "U", "Y", "C"],
+        correctAnswer: "J"
+    },
+    {
+        image: "assets/images/ind_sl_k.webp",
+        answers: ["A", "N", "K", "Z"],
+        correctAnswer: "K"
+    },
+    {
+        image: "assets/images/ind_sl_l.webp",
+        answers: ["L", "J", "V", "W"],
+        correctAnswer: "L"
+    },
+    {
+        image: "assets/images/ind_sl_m.webp",
+        answers: ["W", "M", "N", "B"],
+        correctAnswer: "M"
+    },
+    {
+        image: "assets/images/ind_sl_n.webp",
+        answers: ["M", "N", "U", "V"],
+        correctAnswer: "N"
+    },
+    {
+        image: "assets/images/ind_sl_o.webp",
+        answers: ["Q", "O", "B", "C"],
+        correctAnswer: "O"
+    },
+    {
+        image: "assets/images/ind_sl_p.webp",
+        answers: ["D", "F", "P", "A"],
+        correctAnswer: "P"
+    },
+    {
+        image: "assets/images/ind_sl_q.webp",
+        answers: ["Z", "Q", "G", "R"],
+        correctAnswer: "Q"
+    },
+    {
+        image: "assets/images/ind_sl_r.webp",
+        answers: ["R", "S", "C", "N"],
+        correctAnswer: "R"
+    },
+    {
+        image: "assets/images/ind_sl_s.webp",
+        answers: ["X", "T", "A", "S"],
+        correctAnswer: "S"
+    },
+    {
+        image: "assets/images/ind_sl_t.webp",
+        answers: ["I", "T", "Y", "N"],
+        correctAnswer: "T"
+    },
+    {
+        image: "assets/images/ind_sl_u.webp",
+        answers: ["V", "Y", "O", "U"],
+        correctAnswer: "U"
+    },
+    {
+        image: "assets/images/ind_sl_v.webp",
+        answers: ["U", "V", "Y", "N"],
+        correctAnswer: "V"
+    },
+    {
+        image: "assets/images/ind_sl_w.webp",
+        answers: ["X", "M", "W", "A"],
+        correctAnswer: "w"
+    },
+    {
+        image: "assets/images/ind_sl_x.webp",
+        answers: ["X", "Q", "N", "W"],
+        correctAnswer: "X"
+    },
+    {
+        image: "assets/images/ind_sl_y.webp",
+        answers: ["I", "Y", "Q", "V"],
+        correctAnswer: "Y"
+    },
+    {
+        image: "assets/images/ind_sl_z.webp",
+        answers: ["H", "Z", "E", "K"],
+        correctAnswer: "Z"
+    },
+];

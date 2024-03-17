8102636162307251d9a92f05195a351f2fb5a811
Robert T <robert@tiffert.eu>
2024-03-17 20:35:33 +0000

    Add script to contact that display message once submit and redirect to home page

---
+++ assets/js/contactForm.js
@@ -0,0 +1,39 @@
+/* jshint esversion: 11 */
+
+// Select important elements and link to HTML
+const contactForm = document.getElementById('contact-form');
+const boxContainer = document.querySelector('.box-container');
+const thanksContainer = document.querySelector('.thanks-container');
+
+// Event listener for the form submission
+contactForm.addEventListener('submit', function (event) {
+    // Prevent the form from being submitted normally
+    event.preventDefault();
+
+    // Hide the box container
+    boxContainer.style.display = 'none';
+
+    // Show the thanks container
+    thanksContainer.style.display = 'block';
+
+    // Start the countdown
+    let countdown = 10;
+    const countdownInterval = setInterval(() => {
+        // Update the countdown message
+        thanksContainer.innerHTML = `
+            <div class="display-thanks-message">    
+            <h2>Thank you for contacting us!</h2>
+            <h3>You will be automatically redirected back to the home page in ${countdown}
+            seconds.</h3>
+            </div>
+        `;
+
+        countdown--;
+
+        // When the countdown reaches 0, redirect to the home page and clear the interval
+        if (countdown < 0) {
+            window.location.href = '/../index.html';
+            clearInterval(countdownInterval);
+        }
+    }, 1000);
+});
